ef188690798eba701193a88bb4932e364b32c394
Cristian26121997 <cristian.espina.r1997@gmail.com>
2023-08-31 10:14:42 -0400

    prueba

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Personajes//
+let PuntosDeVidaYugi = 4000
+let PuntosDeVidaKaiba = 4000
+
+//Cartas Yugi//
+let MagoOscuro = 2500
+let SliferElDragonDelCielo = 3000
+let GuardiaCelta = 1400
+
+//Cartas Kaiba//
+let DragonDeOjosAzules = 3000
+let BueyDeBatalla = 1700
+let DragonDelBrillo = 1900
+
+
+//batalla
+if (MagoOscuro, DragonDeOjosAzules){
+    DragonDeOjosAzules > MagoOscuro 
+    console.log ("Daño a yugi " +  -(DragonDeOjosAzules -= MagoOscuro))
+    console.log ("Puntos de vida yugi " + -(DragonDeOjosAzules -= MagoOscuro))
+    console.log ("puntos de vida kaiba " + PuntosDeVidaKaiba)
+}else {
+
+}
+    
+    
+
+
